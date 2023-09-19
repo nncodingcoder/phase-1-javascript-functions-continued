3533509dd6d24c501d726ad990f51a6b1211e5c3
naomiinyele <inyeleinyele@gmail.com>
2023-09-20 01:04:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+return `This Saturday, I want to ${activity}!`
+
+}
+saturdayFun()
+saturdayFun('bathe my dog')
+
+function mondayWork(newActivity = "go to the office") {
+return `This Monday, I will ${newActivity}.`
+}
+mondayWork()
+mondayWork('work from home')
+
+function wrapAdjective(otherHighlight = '*') {
+    return function (highlight = 'special') {
+        return `You are ${otherHighlight}${highlight}${otherHighlight}!`
+    }
+}
+wrapAdjective()('a hard worker')
+wrapAdjective('||')("a dedicated programmer")
